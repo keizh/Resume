90255e0ec07fec9ad73b5c4c802dd9f0ae8b5464
Keizh <krishnamulay05@gmail.com>
2025-02-27 16:40:54 +0530

    done

--- src/components/Page1.jsx
+++ src/components/Page1.jsx
@@ -38,7 +38,7 @@ function Page1() {
           Frontend Full-Stack Software Developer
         </p>
         <a
-          href="https://res.cloudinary.com/dddkhewor/image/upload/v1740654307/Resume_xepwkk.pdfs"
+          href="https://res.cloudinary.com/dddkhewor/image/upload/v1740654307/Resume_xepwkk.pdf"
           target="_blank"
         >
           <Button color="amber" className="">
